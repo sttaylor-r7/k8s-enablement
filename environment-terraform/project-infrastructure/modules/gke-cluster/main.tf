6cb198bd7f01f08c6a9c24d4133408cc6a8d60ea
Stuart Taylor <stuart_taylor@rapid7.com>
2024-07-24 16:35:39 +0100

    Limited ip ranges to connect to GKE API endpoint

--- environment-terraform/project-infrastructure/modules/gke-cluster/main.tf
+++ environment-terraform/project-infrastructure/modules/gke-cluster/main.tf
@@ -21,4 +21,10 @@ resource "google_container_cluster" "lab-clusters" {
     master_ipv4_cidr_block = "172.16.0.0/28"
   }
 
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.0/8"
+      display_name = "Internal"
+    }
+
 }
